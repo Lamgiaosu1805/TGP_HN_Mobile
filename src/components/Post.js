8aa707bf9cd1dc2fa103186546b20a9b724e0e3e
lamgiaosu1999 <nghiemkhaclam185.mta@gmail.com>
2023-01-31 23:12:21 +0700

    improve UI of Post

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,21 +5,46 @@ export default function Post(props) {
     const postData = props.postData;
 
     return (
-        <View style = {styles.highlightPost}>
-            <Image style={styles.img} source={{uri:postData.imageUrl}}/>
-            <Text>{postData.titlePost}</Text>
-            <Text>{postData.metaData_Date}</Text>
+        <View style = {styles.container}>
+            <View style = {styles.highlightPost}>
+                <Image style={styles.img} source={{uri:postData.imageUrl}}/>
+                <Text style={styles.titlePost}>{postData.titlePost}</Text>
+                <Text style={styles.metaData_Date}>{postData.metaData_Date}</Text>
+            </View>
         </View>
+        
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        shadowColor: '#171717',
+        shadowOffset: {
+            width: 4,
+            height: -4
+        },
+        shadowRadius: 3,
+        shadowOpacity: 0.2,
+    },
     highlightPost: {
-        paddingHorizontal: 20
+        paddingHorizontal: 20,
+        marginTop: 8,
+        paddingVertical: 12,
     },
     img: {
         width: Dimensions.get('window').width-40,
         height: Dimensions.get('window').width/3*2-40,
         resizeMode: 'cover',
+    },
+    titlePost: {
+        fontSize: 20,
+        fontWeight: '700',
+        color: '#54595F',
+        marginTop: 12
+    },
+    metaData_Date: {
+        color: '#adadad',
+        fontSize: 13,
+        marginTop: 2,
     }
 })
