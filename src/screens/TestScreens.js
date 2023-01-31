7febfa4db5a04410c312abf2733d017c2127295c
lamgiaosu1999 <nghiemkhaclam185.mta@gmail.com>
2023-02-01 02:32:50 +0700

    add bottom navigation

---
+++ src/screens/TestScreens.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function TestScreens() {
+  return (
+    <View>
+      <Text>TestScreens</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
